614119e96aad257bc906cc77642b455aaaade05f
İrem Atak <b21726933@cs.hacettepe.edu.tr>
2023-09-18 10:34:57 +0300

    First Commit

---
+++ Models/Courses.cs
@@ -0,0 +1,13 @@
+﻿namespace elearningapp.Models
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public int InstructorId { get; set; }
+        public string Category { get; set; }
+        public int EnrollmentCount { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
