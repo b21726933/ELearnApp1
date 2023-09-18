43a21d4c95a3ba04ba7695dd0aff68ac01f14fad
İrem Atak <b21726933@cs.hacettepe.edu.tr>
2023-09-18 17:40:52 +0300

    Adding autherization

---
+++ Models/Assignments.cs
@@ -0,0 +1,11 @@
+﻿namespace elearningapp.Models
+{
+    public class Assignments
+    {
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
---
+++ Models/Enrollments.cs
@@ -0,0 +1,10 @@
+﻿namespace elearningapp.Models
+{
+    public class Enrollments
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CourseId { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
